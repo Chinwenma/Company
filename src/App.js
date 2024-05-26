fbbaabd2b96cbb896c2e28614da90cffbe71cde3
Chinwenma <okoriechinwe1327@gmail.com>
2024-05-26 15:52:31 -0800

    newsletter section

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import About from './components/Layout/About';
 import Blog from './components/Layout/Blog';
 import Counter from './components/Layout/Counter';
 import Hero from './components/Layout/Hero';
+import Newsletter from './components/Layout/Newsletter';
 import Services from './components/Layout/Services';
 import Team from './components/Layout/Team';
 import Testimonial from './components/Layout/Testimonial';
@@ -20,6 +21,7 @@ function App() {
         <Team/>
         <Blog/>,
         <Testimonial/>
+        <Newsletter/>
       
     </>
   );
---
+++ src/components/Layout/Newsletter.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import "../../styles/newsletter.css";
+const Newsletter = () => {
+  return (
+    <section className="newsletter">
+      <div className="container">
+        <div className="newsletter__wrapper">
+          <div className="newsletter__content">
+            <h6 className="subtitle">Newsletter</h6>
+            <h2>
+               Be ahead of the curve
+              <span className="highlight"> get every tech update</span>
+            </h2>
+          </div>
+          <div className="newsletter__form">
+            <input type="email"  placeholder="Enter your Email"/>
+            <button className="secondary__btn">Subscribe</button>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Newsletter;
